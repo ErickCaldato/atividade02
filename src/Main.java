21572118de34095c538b763b698114528bf21bdb
JotaSartor <sartorjoao05@gmail.com>
2024-08-21 20:46:02 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "Caldato";
+
+        if(idadePessoa > 21){
+            System.out.println("é maior que 21");
+        }else{
+            System.out.println("Não é maior");
+        }
+
 
     }
 }
